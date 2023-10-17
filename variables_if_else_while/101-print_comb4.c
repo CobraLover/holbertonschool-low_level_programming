b7fbf538bf40808f42b7411e441dd5c859e8eefe
CobraLover <6716@holbertonstudents.com>
2023-10-17 06:41:41 -0700

    Task.11

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -9,29 +9,27 @@
 
 int main(void)
 {
-        int d, p, q;
+	int d, p, q;
 
-        for (d = '0'; d < '9'; d++)
-        {
-
-        for (p = d + 1; p <= '9'; p++)
-        {
-
+	for (d = '0'; d < '9'; d++)
+	{
+	for (p = d + 1; p <= '9'; p++)
+	{
 	for (q = p + 1; q <= '9'; q++)
 	{
-        if ((p != d) != q)
-        {
-        putchar(d);
-        putchar(p);
+	if ((d != p) != q)
+	{
+	putchar(d);
+	putchar(p);
 	putchar(q);
-        if (d == '7' && p == '8')
-        continue;
-        putchar(',');
-        putchar(' ');
-        }
-        }
-        }
+	if (d == '7' && p == '8')
+		continue;
+	putchar(',');
+	putchar(' ');
 	}
-        putchar('\n');
-        return (0);
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
 }

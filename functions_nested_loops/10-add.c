91f84236c171a53d4e0cf83bfdfe1b4dc5f1bae1
CobraLover <6716@holbertonstudents.com>
2023-10-17 05:16:27 -0700

    Task.10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integrers and returns the result
+ *
+ * @x: number being added
+ *
+ * @y: number being added
+ *
+ * Return: Always 0.
+ *
+ */
+
+int add(int a, int z)
+{
+	return (a + z);
+}

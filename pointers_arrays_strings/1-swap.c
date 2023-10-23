c0e6b6d9eb1d6b42202222a66eb7bb92c55516a4
CobraLover <6716@holbertonstudents.com>
2023-10-23 02:13:46 -0700

    Task.1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swap two integers
+ * @a: first value
+ * @b: second value
+ *
+ * Return: Nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}

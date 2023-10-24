095962d3cf23a1196b370a874cb4fd06dc6df8ac
CobraLover <6716@holbertonstudents.com>
2023-10-24 05:45:56 -0700

    Task.3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,14 +13,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int counter, cmpVal;
+	int count = 0, cmpval;
 
-	counter = 0;
-	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	while (s1[count] == s2[count] && s1[count] != '\0')
 	{
-		counter++;
+		count++;
 	}
-
-	cmpVal = s1[counter] - s2[counter];
-	return (cmpVal);
+	cmpval = s1[count] - s2[count];
+	return (cmpval);
 }

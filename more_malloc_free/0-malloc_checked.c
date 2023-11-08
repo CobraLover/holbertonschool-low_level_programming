a50f6b465a7ae98107cdfc4b023bdb0caff623ae
CobraLover <6716@holbertonstudents.com>
2023-11-08 04:20:54 -0800

    Task.0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,9 +15,6 @@ void *malloc_checked(unsigned int b)
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		fprintf(stderr, "Memory allocation failed\n");
 		exit(98);
-	}
 	return (ptr);
 }

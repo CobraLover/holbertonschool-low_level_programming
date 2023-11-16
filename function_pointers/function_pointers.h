3da08137dc08f90d4a4942f04cc2b1d35fdfc6be
CobraLover <6716@holbertonstudents.com>
2023-11-16 00:27:35 -0800

    function_pointers.h

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+int int_index(int *array, int size, int (*cmp)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif

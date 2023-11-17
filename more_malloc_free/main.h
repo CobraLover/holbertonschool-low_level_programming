18e7d71b8812cac1adcdb9283ff99865f1029773
CobraLover <6716@holbertonstudents.com>
2023-11-17 02:18:57 -0800

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,15 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _strlen(char *s);
-char *checkarg(char *s);
-char *_mul(char *s1, char *s2);
-int main(int argc, char *argv[]);
-void *_calloc(unsigned int nmb, unsigned int size);
-char *makesecond(char *src, int l1, char c, int zero);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #include <stdio.h>
 #include <stdlib.h>
+
 int _putchar(char c);
 int *array_range(int min, int max);
 void *malloc_checked(unsigned int b);

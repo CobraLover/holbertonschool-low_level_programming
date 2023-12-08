3c4529dc65449e53e35ab6a01db5d8c4370424ee
CobraLover <6716@holbertonstudents.com>
2023-12-08 00:58:18 -0800

    Task.0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
-	unsigned long int i;
+	unsigned long int i = 0;
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
